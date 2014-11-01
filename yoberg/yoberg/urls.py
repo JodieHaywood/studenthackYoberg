5fa571d65cbb18feec58bc4d392d18310cb54e46
Devin Ridgway <devin.ridgway@gmail.com>
2014-11-01 03:23:28 +0000

    Made some changes to add in some Yo.

--- yoberg/yoberg/urls.py
+++ yoberg/yoberg/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', 'yoscribe.views.home', name='home'),
+    url(r'^yo/', 'yoscribe.views.yo', name='yo'),
     url(r'^admin/', include(admin.site.urls)),
 )
--- yoberg/yoscribe/models.py
+++ yoberg/yoscribe/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Yoscriber(models.Model):
+    yoname = models.CharField(max_length=200)
+    phonenumber = models.CharField(max_length=15)
