98ddffb71b37039728f3272b33cd01b0d529eeca
Dan Underwood <underwd2@cs.man.ac.uk>
2014-11-01 23:47:28 +0000

    Adding new field finding function

--- yoberg/bloomberg/Bloomberg.py
+++ yoberg/bloomberg/Bloomberg.py
@@ -1,8 +1,10 @@
 import blpapi
 
-def getFields(company):
+def getFieldValues(company, fields):
+
         host = "10.8.8.1"
         port = 8194
+        REFDATA_SVC = "//blp/refdata"
 
         sessionOptions = blpapi.SessionOptions()
         sessionOptions.setServerHost(host)
@@ -17,21 +19,18 @@ def getFields(company):
         if not session.start():
                 print "Failed to start session."
 
-        if not session.openService("//blp/refdata"):
+        if not session.openService(REFDATA_SVC):
                 print "Failed to open //blp/refdata"
 
-        refDataService = session.getService("//blp/refdata")
+        refDataService = session.getService(REFDATA_SVC)
         request = refDataService.createRequest("ReferenceDataRequest")
 
         # append security to request
         request.append("securities", company + " US Equity")
 
         # append fields to request
-        request.append("fields", "PX_LAST")
-        request.append("fields", "DS002")
-        request.append("fields", "DS003")
-        request.append("fields", "DS004")
-        request.append("fields", "DS005")
+        for field in fields:
+                request.append("fields", field)
 
         print "Sending Request:", request
         session.sendRequest(request)
@@ -50,6 +49,69 @@ def getFields(company):
                                 for msg in ev:
                                         for element in msg.asElement().getElement("securityData").getValue().getElement("fieldData").elements():
                                                 data[element.name().__str__()] = element.getValue()
+                                return data
+        finally:
+                # Stop the session
+                session.stop()
+
+
+def getFields(text):
+        host = "10.8.8.1"
+        port = 8194
+        APIFLDS_SVC = "//blp/apiflds"
+        FIELD_ID = blpapi.Name("id")
+        FIELD_MNEMONIC = blpapi.Name("mnemonic")
+        FIELD_DATA = blpapi.Name("fieldData")
+        FIELD_DESC = blpapi.Name("description")
+        FIELD_INFO = blpapi.Name("fieldInfo")
+        FIELD_ERROR = blpapi.Name("fieldError")
+        FIELD_MSG = blpapi.Name("message")
+
+        # Fill SessionOptions
+        sessionOptions = blpapi.SessionOptions()
+        sessionOptions.setServerHost(host)
+        sessionOptions.setServerPort(port)
+
+        print "Connecting to %s:%d" % (host, port)
+
+        # Create a Session
+        session = blpapi.Session(sessionOptions)
+
+        # Start a Session
+        if not session.start():
+                print "Failed to start session."
+                return
+
+        if not session.openService(APIFLDS_SVC):
+                print "Failed to open", APIFLDS_SVC
+                return
+
+        fieldInfoService = session.getService(APIFLDS_SVC)
+        request = fieldInfoService.createRequest("FieldSearchRequest")
+        request.set("searchSpec", text)
+        exclude = request.getElement("exclude")
+        exclude.setElement("fieldType", "Static")
+        request.set("returnFieldDocumentation", False)
+
+        print "Sending Request:", request
+        session.sendRequest(request)
+
+        try:
+                # Process received events
+                while(True):
+                        # We provide timeout to give the chance to Ctrl+C handling:
+                        ev = session.nextEvent(500)
+                        data = []
+                        if ev.eventType() != blpapi.Event.RESPONSE and \
+                                ev.eventType() != blpapi.Event.PARTIAL_RESPONSE:
+                                continue
+                        for msg in ev:
+                                for field in msg.getElement("fieldData").values():
+                                        fldInfo = field.getElement(FIELD_INFO)
+                                        data.append([field.getElementAsString(FIELD_ID), fldInfo.getElementAsString(FIELD_MNEMONIC), fldInfo.getElementAsString(FIELD_DESC)])
+
+                        # Response completly received, so we could exit
+                        if ev.eventType() == blpapi.Event.RESPONSE:
                                 return data
         finally:
                 # Stop the session
--- yoberg/bloomberg/RandomCompany.py
+++ yoberg/bloomberg/RandomCompany.py
@@ -10,7 +10,7 @@ def getRandomCompanyResponse():
                 for row in linereader:
                         companies.append(row[0])
 
-                result = Bloomberg.getFields(random.choice(companies))
+                result = Bloomberg.getFieldValues(random.choice(companies), ["PX_LAST", "DS002", "DS004"])
 
                 strResult = str(result['DS002']) + " last priced at: " + str(result['PX_LAST']) + " " + str(result['DS004'])
                 return strResult
