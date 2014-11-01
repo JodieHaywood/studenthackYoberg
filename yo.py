59aa517a602043d1b590dda719f9f4dbb1d4a352
Devin Ridgway <devin.ridgway@gmail.com>
2014-11-01 00:07:23 +0000

    tiny change to yo script

--- yo.py
+++ yo.py
@@ -1,11 +1,13 @@
 import requests
 
+api_key = '35d45ae0-6bc0-4782-9374-5404463fccbc'
+
 def yo_all():
-  payload = {'api_token': '35d45ae0-6bc0-4782-9374-5404463fccbc'}
+  payload = {'api_token': api_key}
   r = requests.post('http://api.justyo.co/yoall/', data=payload)
 
 def yo_all_link(link):
-  payload = {'api_token': '35d45ae0-6bc0-4782-9374-5404463fccbc', 'link': link}
+  payload = {'api_token': api_key, 'link': link}
   r = requests.post('http://api.justyo.co/yoall/', data=payload)
 
 
